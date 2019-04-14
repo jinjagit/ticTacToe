214e4d062f1a9e7862edcb9f9cf3bdaa5ffdd180
Simon Tharby <jinjamail@gmail.com>
2019-04-14 21:49:29 +0100

    basic mouseover and click events for grid cells

--- main.js
+++ main.js
@@ -5,42 +5,80 @@ function render() {
     side = window.innerWidth * 0.8;
   }
 
-  if (side > 604) { side = 604; }
-
-  board.style.height = `${side}px`;
-  board.style.width = `${side}px`;
-  board.style.gap = `${side/70}px`
-
+  if (side > 600) { side = 600; }
+  let gap = Math.floor(side / 70);
   let fontSize = side / 5;
+  side = Math.floor(side - ((2 * gap) / 3) * 3);
+
+  grid.style.height = `${side}px`;
+  grid.style.width = `${side}px`;
+  grid.style.gap = `${gap}px`
 
   // remove cells from board before (re)creating
   let cells = document.getElementsByClassName('cell');
 
   for (let i = cells.length - 1; i >= 0; i--) {
-    board.removeChild(cells[i]);
+    grid.removeChild(cells[i]);
   }
 
-  // draw & style; new board cells & content
+  // Create new board cells & content + behaviours
   for (i = 0; i < 9; i++) {
     const cell = document.createElement('div');
     cell.id = `${i}`;
     cell.classList.add('cell');
-    cell.style.lineHeight = `${(side - 4) / 3}px`;
-    const data = document.createElement('p');
-    data.id = `data${i}`;
-    data.classList.add('data');
-    data.style.fontSize = `${fontSize}px`;
-    data.innerHTML = layout[i];
-    cell.appendChild(data);
-    board.appendChild(cell);
+    cell.style.lineHeight = `${(side - (2 * gap)) / 3}px`;
+    cell.addEventListener("mouseover", function() {
+      highlight(this.id);
+    });
+    cell.addEventListener("mouseout", function() {
+      cell.style.backgroundColor = 'white';
+    });
+    cell.addEventListener("click", function() {
+      selectCell(this.id);
+    });
+    const piece = document.createElement('p');
+    piece.id = `piece${i}`;
+    piece.classList.add('piece');
+    piece.style.fontSize = `${fontSize}px`;
+    piece.innerHTML = layout[i];
+    cell.appendChild(piece);
+    grid.appendChild(cell);
   }
 }
 
+function highlight(index) {
+  let piece = document.getElementById(`piece${index}`);
+  if (piece.innerHTML == '') {
+    let cell = document.getElementById(`${index}`);
+    cell.style.backgroundColor = 'red';
+  }
+}
+
+function selectCell(index) {
+  console.log(`cell index ${index} clicked`);
+}
+
+// Board Module:
+const board = (() => {
+  var state = ['X', '', 'O', '', '', '', '', '', ''];
+  const add = (piece, index) => {
+    if (state[index] == '') { state[index] = piece; }
+  };
+  const reset = () => { state.fill(''); };
+  return { add, state, reset };
+})();
+
 let side = 600;
-let board = document.getElementById('board');
+let grid = document.getElementById('grid');
 
-let layout = ['O', '', 'X', 'X', 'O', '', 'O', '', 'X'];
+let layout = board.state;
+
+render();
 
 
+board.add('X', 1);
+board.add('O', 4);
+board.add('X', 2);
+board.add('O', 7);
 
 render();
